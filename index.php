de5f4f867d9b0fd2ccb6500f4d3c9ffb56ae7305
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:22:12 +0200

    php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo2</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $name = 'cyril';
+    $firstname = 'jardin';
+    $age = '18';
+    echo "$name \n";
+    echo "$firstname \n";
+    echo "$age \n";
+     ?>
+   </p>
+  </body>
+</html>
